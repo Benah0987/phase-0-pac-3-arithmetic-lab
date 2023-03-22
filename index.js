4fbf60ddc45c1f93a7282c66a5b28d0f91ce6fd8
Benah0987 <88454033+Benah0987@users.noreply.github.com>
2023-03-22 12:43:45 +0300

    Update index.js
    co

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(x,y) {
+    
+    return x + y;
+}
+
+const sum = add(7,4);
+console.log(sum);
+
+//multiplication
+function multipy(x,y) {
+    
+    return x * y;
+}
+
+const times = multipy(7,4);
+console.log(times);
+
+
+//subtraction
+function minus(x,y) {
+    
+    return x - y;
+}
+
+const minus = multipy(7,4);
+console.log(times);
+
+//division
+function division(x,y) {
+    
+    return x / y;
+}
+
+const divide = multipy(7,4);
+parseFloat(divide);
+console.log(divide)
